cebe45f508825e37ab1581fae155c772661b5104
Mochamad Akbar <mochakbar912@gmail.com>
2024-08-28 17:20:02 +0700

    add custom errors

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.19;
 
+error Raffle__SendMoreToEnterRaffle();
+
 /**
  * @title A sample Raffle contract
  * @author auxulry
@@ -10,12 +12,17 @@ pragma solidity ^0.8.19;
  */
 contract Raffle {
     uint256 private immutable i_enteranceFee;
+    address payable[] private s_players;
 
     constructor(uint256 enteranceFee) {
         i_enteranceFee = enteranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        if (msg.value < i_enteranceFee) revert Raffle__SendMoreToEnterRaffle();
+
+        s_players.push(payable(msg.sender));
+    }
 
     function pickWinner() public {}
 
